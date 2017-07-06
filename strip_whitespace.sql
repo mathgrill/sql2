3a7f2628b279f035a3d7158988fcbf2c8cacfd6b
mathgrill <carlo.baluta@gmail.com>
2017-07-06 06:24:13 -0400

    Update strip_whitespace.sql

--- strip_whitespace.sql
+++ strip_whitespace.sql
@@ -4,3 +4,9 @@ SELECT   col, REGEXP_REPLACE (col, '(^[[:space:]]+)|([[:space:]]+$)', NULL) new_
 -- replace all whitespace with spaces, multiple contiguous whitespace character with a single space, remove space at ends of the string
 -- https://stackoverflow.com/questions/27607353/oracle-update-multiple-spaces-in-a-column-with-a-single-space
 SELECT TRIM(REGEXP_REPLACE(mycolumn, '([[:space:]]{1,})', ' ')) col from mytable;
+
+-- using translate
+-- https://stackoverflow.com/questions/16407135/oracle-regexp-to-replace-n-r-and-t-with-space
+select translate(your_column, chr(10)||chr(11)||chr(13), '    ')
+from your_table;
+-- This replaces newline, tab and carriage return with space.
