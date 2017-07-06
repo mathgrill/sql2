a96d4250f39beded0734cc0a30b4b73aa2c59154
mathgrill <carlo.baluta@gmail.com>
2017-07-06 06:17:47 -0400

    added a variant

--- strip_whitespace_at_both_ends_of_string.sql
+++ strip_whitespace_at_both_ends_of_string.sql
@@ -1,1 +1,4 @@
 SELECT   col, REGEXP_REPLACE (col, '(^[[:space:]]+)|([[:space:]]+$)', NULL) new_col_value, TRIM (col) FROM test_whitespace;
+
+--replace all whitespace with spaces, multiple contiguous whitespace character with a single space, remove space at ends of the string
+SELECT TRIM(REGEXP_REPLACE(mycolumn, '([[:space:]]{1,})', ' ')) col from mytable;
