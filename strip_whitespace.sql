a54681a7e360ea9dded001b869188b7d8f1ae73a
mathgrill <carlo.baluta@gmail.com>
2017-07-06 06:21:32 -0400

    renamed file - added comments

--- strip_whitespace.sql
+++ strip_whitespace.sql
@@ -1,5 +1,6 @@
 -- remove whitespace at both ends of a string
 SELECT   col, REGEXP_REPLACE (col, '(^[[:space:]]+)|([[:space:]]+$)', NULL) new_col_value, TRIM (col) FROM test_whitespace;
 
---replace all whitespace with spaces, multiple contiguous whitespace character with a single space, remove space at ends of the string
+-- replace all whitespace with spaces, multiple contiguous whitespace character with a single space, remove space at ends of the string
+-- https://stackoverflow.com/questions/27607353/oracle-update-multiple-spaces-in-a-column-with-a-single-space
 SELECT TRIM(REGEXP_REPLACE(mycolumn, '([[:space:]]{1,})', ' ')) col from mytable;
