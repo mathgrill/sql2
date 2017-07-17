f26ce575ff6d46ab5db57a84411f7f6349ca0b29
mathgrill <carlo.baluta@gmail.com>
2017-07-17 09:01:05 -0400

    useful links

---
+++ role.sql
@@ -0,0 +1,7 @@
+-- http://www.java2s.com/Tutorial/Oracle/0700__User-Privilege/GrantingPrivilegestoRoles.htm
+-- https://www.techonthenet.com/oracle/grant_revoke.php
+-- http://psoug.org/reference/roles.html
+
+SELECT * FROM role_sys_privs;
+
+DESC role_sys_privs;
