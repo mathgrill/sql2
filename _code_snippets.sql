2faa1335060ca917ce08a33d90d227fa9ae25f21
mathgrill <carlo.baluta@gmail.com>
2019-10-19 08:07:26 -0400

    Update _code_snippets.sql

--- _code_snippets.sql
+++ _code_snippets.sql
@@ -2,3 +2,10 @@ ALTER TABLE supplier
 ADD CONSTRAINT supplier_pk PRIMARY KEY (supplier_id);
 
 select * from all_tab_cols where owner = 'OWNER' and table_name = 'TABLE_NAME';
+
+select * from ALL_COMMENTS;
+
+COMMENT ON COLUMN employees.job_id IS 'abbreviated job title';
+
+str_comment:='COMMENT ON COLUMN '||rec.table_name||'.'||rec.column_name
+            ||' IS '''||REPLACE( rec.description,'''','''''')||'''; ' ;
